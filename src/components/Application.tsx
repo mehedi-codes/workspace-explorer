76b0bf2d6c1cec04bff0093b5b9e81704322cde6
Mehedi Hasan <mehedi@etlimited.net>
2026-09-22 02:40:46 +0600

    chore: scaffold vite react typescript project

---
+++ src/components/Application.tsx
@@ -0,0 +1,6 @@
+export const Application = () => {
+    return <main className="min-h-dvh flex flex-col gap-y-2 justify-center items-center bg-black">
+        <img src="/favicon.svg" alt="logo" width={64} height={64} />
+        <h2 className="text-white">Workspace Explorer</h2>
+    </main>
+}
